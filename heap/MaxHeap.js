ff8685979eb9f4f5ccb6db87d63fd86217c32a5e
ayushikabansal <ayushikabansal7890@gmail.com>
2026-05-23 12:34:30 +0530

    chore: add max heap implementation

--- heap/MaxHeap.js
+++ heap/MaxHeap.js
@@ -1,4 +1,4 @@
-class MinHeap {
+class MaxHeap {
   constructor() {
     this.heap = [];
   }
@@ -14,46 +14,38 @@ class MinHeap {
   swap(i, j) {
     [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
   }
-
-  // O(logn)
   insert(val) {
     this.heap.push(val);
     let i = this.heap.length - 1;
     while (i > 0) {
       const parentIndex = this.parent(i);
-      if (this.heap[parentIndex] <= this.heap[i]) break;
+      if (this.heap[parentIndex] >= this.heap[i]) break;
       this.swap(parentIndex, i);
       i = parentIndex;
     }
   }
-
-  // O(logn)
-  extractMin() {
+  extractMax() {
     if (this.heap.length === 0) return null;
     if (this.heap.length === 1) return this.heap.pop();
 
-    const min = this.heap[0];
+    const max = this.heap[0];
     this.heap[0] = this.heap.pop();
-    this.minHeapify(0);
-    return min;
+    this.maxHeapify(0);
+    return max;
   }
-
-  /* Instead of recursive, implement iterative solution
-   * beacuse in case of large heaps, there are high chances
-   * of hitting javascript call stack limit
-   */
-  minHeapify(i) {
+  maxHeapify(i) {
     const n = this.heap.length;
     while (true) {
-      let smallest = i;
+      let largest = i;
       let l = this.left(i);
       let r = this.right(i);
 
-      if (l < n && this.heap[l] < this.heap[smallest]) smallest = l;
-      if (r < n && this.heap[r] < this.heap[smallest]) smallest = r;
-      if (i === smallest) break;
-      this.swap(i, smallest);
-      i = smallest;
+      if (l < n && this.heap[l] > this.heap[largest]) largest = l;
+      if (r < n && this.heap[r] > this.heap[largest]) largest = r;
+
+      if (largest === i) break;
+      this.swap(i, largest);
+      i = largest;
     }
   }
 }
