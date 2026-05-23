f0d8e627c10361fe3b9f23488f45c795488b7966
ayushikabansal <ayushikabansal7890@gmail.com>
2026-05-23 13:11:37 +0530

    chore: add custom heap implementation

--- heap/Heap.js
+++ heap/Heap.js
@@ -1,6 +1,7 @@
-class MinHeap {
-  constructor() {
+class Heap {
+  constructor(comparator) {
     this.heap = [];
+    this.cmp = comparator;
   }
   parent(i) {
     return Math.floor((i - 1) / 2);
@@ -14,49 +15,40 @@ class MinHeap {
   swap(i, j) {
     [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
   }
-
-  // O(logn)
   insert(val) {
     this.heap.push(val);
     let i = this.heap.length - 1;
     while (i > 0) {
       const parentIndex = this.parent(i);
-      if (this.heap[parentIndex] <= this.heap[i]) break;
+      if (this.cmp(this.heap[parentIndex], this.heap[i]) <= 0) break;
       this.swap(parentIndex, i);
       i = parentIndex;
     }
   }
-
-  // O(logn)
-  extractMin() {
+  extract() {
     if (this.heap.length === 0) return null;
     if (this.heap.length === 1) return this.heap.pop();
 
-    const min = this.heap[0];
+    const ans = this.heap[0];
     this.heap[0] = this.heap.pop();
-    this.minHeapify(0);
-    return min;
+    this.heapify(0);
+    return ans;
   }
-
-  /* Instead of recursive, implement iterative solution
-   * beacuse in case of large heaps, there are high chances
-   * of hitting javascript call stack limit
-   */
-  minHeapify(i) {
+  heapify(i) {
     const n = this.heap.length;
     while (true) {
-      let smallest = i;
+      let best = i;
       let l = this.left(i);
       let r = this.right(i);
 
-      if (l < n && this.heap[l] < this.heap[smallest]) smallest = l;
-      if (r < n && this.heap[r] < this.heap[smallest]) smallest = r;
-      if (i === smallest) break;
-      this.swap(i, smallest);
-      i = smallest;
+      if (l < n && this.cmp(this.heap[l], this.heap[best]) <= 0) best = l;
+      if (r < n && this.cmp(this.heap[r], this.heap[best]) <= 0) best = r;
+
+      if (i === best) break;
+      this.swap(i, best);
+      i = best;
     }
   }
-
   peek() {
     if (this.heap.length === 0) return null;
     return this.heap[0];
